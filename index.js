df1e92794facf70e719f58331b62d76cf4e7d79c
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-11 20:26:50 +0600

    map filter and find use properly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let actors = [
     {id : 2131, nam : "Adnan Sami"},
-    {id : 2131, nam : "Onil Kapur"},
-    {id : 2131, nam : "Layla o Layla"},
+    {id : 2165, nam : "Onil Kapur"},
+    {id : 2145, nam : "Layla o Layla"},
 ];
 
 let totalActors = [];
@@ -15,5 +15,11 @@ console.log(totalActors);
 
 
 const allNames = actors.map(x => x.nam);
+const allActorIds = actors.map(x => x.id);
+const allBigActors = actors.filter(x => x.id > 2140);
+const allBigActor = actors.find(x => x.id > 2140);
 console.log(allNames);
+console.log(allActorIds);
+console.log(allBigActors);
+console.log(allBigActor);
 
